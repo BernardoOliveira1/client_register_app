afe2bff05cf4dca7b514ee872bd03060eff731fa
BRO.Oliveira <bernardo.rocha.oliveira@gmail.com>
2022-02-21 12:41:58 -0300

    initial_design: finishes register page design

--- lib/presentation/pages/register_page.dart
+++ lib/presentation/pages/register_page.dart
@@ -8,15 +8,74 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: const Center(child: Text('Cadastro')),
+      ),
       backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            _RegisterTextField(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            const _RegisterTextField(
+              labelText: 'Matrícula',
+              hintText: 'Digite a Matrícula',
+              maxLength: 11,
+            ),
+            const _RegisterTextField(
               labelText: 'CPF',
               hintText: 'Digite o CPF',
               maxLength: 11,
+            ),
+            const _RegisterTextField(
+              labelText: 'E-mail',
+              hintText: 'Digite o E-mail',
+              maxLength: 11,
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    side: MaterialStateProperty.all<BorderSide>(
+                      BorderSide(
+                        width: mediaQuery.size.width * (1.2 / 360),
+                        color: Colors.blue,
+                      ),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(mediaQuery.size.width * (9 / 360)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: SizedBox(
+                    height: mediaQuery.size.width * 0.1,
+                    child: Center(
+                      child: Text(
+                        'Cadastrar',
+                        style: TextStyle(
+                          fontSize: mediaQuery.size.width * (18 / 360),
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Flexible(
+              child: SizedBox(
+                height: 20,
+              ),
             ),
           ],
         ),
@@ -47,6 +106,10 @@ class _RegisterTextField extends HookWidget {
         controller: null,
         focusNode: _nameFocusNode,
         textCapitalization: TextCapitalization.sentences,
+        style: const TextStyle(
+          fontSize: 18,
+          color: Colors.blue,
+        ),
         textInputAction: TextInputAction.next,
         decoration: InputDecoration(
           enabledBorder: const OutlineInputBorder(
