8eadeb1f7e8ca1e803fabce180b9b118af0c1b7a
BRO.Oliveira <bernardo.rocha.oliveira@gmail.com>
2022-02-23 19:24:16 -0300

    application: adds register controller

---
+++ lib/application/register_controller.dart
@@ -0,0 +1,71 @@
+import 'package:client_register_app/domain/client.dart';
+import 'package:client_register_app/domain/core/value_objects.dart';
+import 'package:client_register_app/domain/i_repository.dart';
+import 'package:client_register_app/presentation/widgets/message.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class RegisterController extends GetxController {
+  RegisterController(
+    this._clientRepository,
+  );
+
+  final IClientRepository _clientRepository;
+
+  final cpf = Rxn<CPF>();
+  final email = Rxn<Email>();
+  final registrationCode = Rxn<RegistrationCode>();
+  final cpfTextEditingController = TextEditingController();
+  final emailTextEditingController = TextEditingController();
+  final registrationCodeTextEditingController = TextEditingController();
+  final showCPFValueFailure = RxBool(false);
+  final showEmailValueFailure = RxBool(false);
+  final showRegistrationCodeValueFailure = RxBool(false);
+
+  void finishRegister() {
+    final client = _buildClient();
+    createClientDoc(client);
+  }
+
+  Client _buildClient() {
+    return Client(
+      registrationCode: registrationCode.value!,
+      cpf: cpf.value!,
+      email: email.value!,
+    );
+  }
+
+  void _success() {
+    cpf.value = null;
+    email.value = null;
+    registrationCode.value = null;
+    cpfTextEditingController.clear();
+    emailTextEditingController.clear();
+    registrationCodeTextEditingController.clear();
+    showMessage(Get.context!,
+        title: 'Operação Bem Sucedida',
+        message: 'Cliente adicionado ao banco de dados',
+        color: Colors.green);
+  }
+
+  void _failure() {
+    showMessage(Get.context!,
+        title: 'Algo deu errado',
+        message:
+            'Tente novamente!\nCPF deve ter 11 dígitos\nMatrícula deve ter 5 dígitos\nEmail deve ser válido',
+        color: Colors.red);
+  }
+
+  Future<void> createClientDoc(Client client) async {
+    if (client.isValid()) {
+      await _clientRepository.create(client).then(
+            (value) => value.fold(
+              (l) => _failure(),
+              (r) => _success(),
+            ),
+          );
+    } else {
+      _failure();
+    }
+  }
+}
