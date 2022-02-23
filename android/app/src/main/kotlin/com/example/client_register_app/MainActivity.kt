5f958d5dfc4bb7136150eb374eca7a8361fc7681
BRO.Oliveira <bernardo.rocha.oliveira@gmail.com>
2022-02-23 15:41:12 -0300

    repository: configures firebase

--- android/app/src/main/kotlin/com/example/client_register_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/client_register_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.client_register_app
+package com.example.client.register.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:client_register_app/presentation/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(const RegisterApp());
 }
 
