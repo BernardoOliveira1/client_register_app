d1707af2dac2a4f4efa7bfea11a79ebc6087f6d2
BRO.Oliveira <bernardo.rocha.oliveira@gmail.com>
2022-02-23 22:45:35 -0300

    get_all_clients: adds search controller

---
+++ lib/application/search_controller.dart
@@ -0,0 +1,25 @@
+import 'package:client_register_app/domain/client.dart';
+import 'package:client_register_app/domain/core/failure.dart';
+import 'package:client_register_app/domain/i_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:get/get.dart';
+
+class SearchController extends GetxController with StateMixin {
+  SearchController(
+    this._clientRepository,
+  );
+
+  final IClientRepository _clientRepository;
+
+  final clients = Rxn<List<Either<ClientFailure, Client>>>();
+
+  @override
+  void onInit() {
+    super.onInit();
+    _getAllData();
+  }
+
+  void _getAllData() async {
+    clients.value = await _clientRepository.getAll();
+  }
+}
